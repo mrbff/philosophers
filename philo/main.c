562eac4e431c9b37047a4aa03c85bf41d2ccd73d
mrbff <marco.baffo@live.it>
2023-01-29 19:52:52 +0100

    t

--- philo/main.c
+++ philo/main.c
@@ -1,9 +1,38 @@
 #include "philo.h"
-
+/*
 int	main(int ac, char **ac)
 {
 	t_param	philo;
 
 	ft_check(ac, av, &philo);
 
+}*/
+/*
+void	*eat(void	*arg)
+{
+	
+}
+
+void	*sleep()
+
+void	*repeat()
+*/
+int	main(int ac, char **av)
+{
+	pthread_t *ph;
+
+	ph = phialloc();
+
+	i = -1;
+	while (++i < nphi)
+	{
+		if (pthread_create(&ph[i], NULL/*where return*/, &eat, NULL/*arg*/))
+			ft_putstr_fd("Failed to create thread\n", 2);
+	}
+	i = -1;
+	while (++i < nphi)
+	{
+		if (pthread_join(ph[i], NULL))
+			ft_putstr_fd("Failed to join thread\n", 2);
+	}
 }
